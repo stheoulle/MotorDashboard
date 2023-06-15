3d7dad78832d6c8d91a9a40f8ccbdf6fee0fa931
Sarah Theoulle <s.theoulle@outlook.fr>
2023-06-15 10:53:57 +0200

    frontend: add "endloop" message on WS
    for scripting this message is sent to the WebClient for each loop.

--- frontend/src/app/receipe-list/receipe-list.component.ts
+++ frontend/src/app/receipe-list/receipe-list.component.ts
@@ -40,6 +40,7 @@ export class ReceipeListComponent {
         console.log(this.app.receipelistitem[j], typeof(this.app.receipelistitem[j]));
         this.app.sendMessageList(this.app.receipelistitem[j]);
      }
+     this.ws.sendMessage("endloop");
     }
     
 
